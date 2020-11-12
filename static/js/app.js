c667afda239f7ba48c292adf11ebf5b622c832ad
adam mak <2283-AdamMaksimovich@users.noreply.nu.bootcampcontent.com>
2020-11-11 23:01:51 -0600

    push

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@
 
 // Use D3 fetch to read the JSON file
 // The data from the JSON file is arbitrarily named importedData as the argument
-d3.json("./samples.json").then((bbdata) => {
+d3.json("samples.json").then((bbdata) => {
 
     // array
 //    var labels = Object.values(bbdata.names);
